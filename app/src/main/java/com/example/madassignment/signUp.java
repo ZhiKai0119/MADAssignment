31938ab6431cac2d12709f9c7b5d736a9700dd97
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2020-08-14 20:00:53 +0800

    commit by zhikai

--- app/src/main/java/com/example/madassignment/signUp.java
+++ app/src/main/java/com/example/madassignment/signUp.java
@@ -3,9 +3,11 @@ package com.example.madassignment;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +16,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class signUp extends AppCompatActivity {
 
@@ -28,6 +39,12 @@ public class signUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser mUser;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+
+//    private FirebaseFirestore mStore;
+//    private String userId;
+    private static final String TAG = "signUp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +59,10 @@ public class signUp extends AppCompatActivity {
         goLogin = (TextView) findViewById(R.id.Go_Login);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase  =FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("Users");
+
+//        mStore = FirebaseFirestore.getInstance();
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -65,7 +86,12 @@ public class signUp extends AppCompatActivity {
                 String email = memail.getText().toString().trim();
                 String password = mpassword.getText().toString().trim();
                 String confirmPwd = mconfirmPwd.getText().toString().trim();
+                String Name = mname.getText().toString().trim();
 
+                if(TextUtils.isEmpty(Name)) {
+                    mname.setError("Name is Required!");
+                    return;
+                }
                 if(TextUtils.isEmpty(email)) {
                     memail.setError("Email is Enquired!");
                     return;
@@ -89,14 +115,72 @@ public class signUp extends AppCompatActivity {
                 }
                 progressBar.setVisibility(View.VISIBLE);
 
+//                mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                        if (authResult != null) {
+//                            String userId = mAuth.getCurrentUser().getUid();
+//                            DatabaseReference currentUserDb = mDatabaseReference.child(userId);
+//                            currentUserDb.child("Name").setValue(mname);
+//                            currentUserDb.child("Email").setValue(memail);
+//                            currentUserDb.child("Image").setValue("none");
+//
+//                            Intent intent = new Intent(signUp.this, MainPage.class);
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                            startActivity(intent);
+//
+//                        }else {
+//                            Toast.makeText(signUp.this, "Error!!!", Toast.LENGTH_LONG).show();
+//                            progressBar.setVisibility(View.GONE);
+//                        }
+//                    }
+//                });
+
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(signUp.this, "User Created.", Toast.LENGTH_LONG).show();
-                            startActivity(new Intent(signUp.this, MainPage.class));
+
+                            FirebaseUser laccUser = mAuth.getCurrentUser();
+                            laccUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(signUp.this, "Verification Email Has Been Sent.", Toast.LENGTH_LONG).show();
+                                    mname.setText("");
+                                    memail.setText("");
+                                    mpassword.setText("");
+                                    mconfirmPwd.setText("");
+                                    startActivity(new Intent(signUp.this, Login.class));
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: Email not sent" + e.getMessage());
+                                }
+                            });
+
+
+//                            Toast.makeText(signUp.this, "User Created.", Toast.LENGTH_LONG).show();
+//                            userId = mAuth.getCurrentUser().getUid();
+//                            DocumentReference documentReference = mStore.collection("users").document(userId);
+//                            Map<String, Object> user = new HashMap<>();
+//                            user.put("Name", mname);
+//                            user.put("email", memail);
+//                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                                @Override
+//                                public void onSuccess(Void aVoid) {
+//                                    Log.d(TAG, "onSuccess: user profile is created for " + userId);
+//                                }
+//                            }).addOnFailureListener(new OnFailureListener() {
+//                                @Override
+//                                public void onFailure(@NonNull Exception e) {
+//                                    Log.d(TAG, "onFailure: " + e.toString());
+//                                }
+//                            });
+//                            startActivity(new Intent(signUp.this, MainPage.class));
                         }else {
                             Toast.makeText(signUp.this, "Error!!!" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
@@ -107,6 +191,7 @@ public class signUp extends AppCompatActivity {
         goLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 startActivity(new Intent(signUp.this, Login.class));
             }
         });
