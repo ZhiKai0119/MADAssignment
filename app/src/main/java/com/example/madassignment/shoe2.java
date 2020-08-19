288c25666a8da7db0908ba186ab36a38842328fb
Yuetfoong0817 <leeyf-wm19@student.tarc.edu.my>
2020-08-19 11:26:17 +0800

    by yf 11:25am 19/8/2020

---
+++ app/src/main/java/com/example/madassignment/shoe2.java
@@ -0,0 +1,30 @@
+package com.example.madassignment;
+
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class shoe2 extends AppCompatActivity {
+
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoe2);
+
+        Spinner spinner = (Spinner) findViewById(R.id.sizeSpinnerShoe2);
+        String[] sizes = {"XS", "S", "M", "L", "XL", "XXL"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sizes);
+        spinner.setAdapter(adapter);
+
+
+
+    }
+
+}
