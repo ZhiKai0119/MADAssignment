b101eabe5f0be2f9151acce440998fd420f52e9e
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2020-08-20 23:25:40 +0800

    commit by zhikai

--- app/src/main/java/com/example/madassignment/shoe1.java
+++ app/src/main/java/com/example/madassignment/shoe1.java
@@ -1,30 +1,141 @@
 package com.example.madassignment;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 
 public class shoe1 extends AppCompatActivity {
 
-
+    private FloatingActionButton addCartBtn;
+    private TextView productPriceWomen, productName, productDesc;
+    private ImageView imageShoe;
     private Spinner spinner;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference mReference;
+    private StorageReference storageReference;
+    public long maxId = 0;
+    public long quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoe1);
 
-        Spinner spinner = (Spinner) findViewById(R.id.sizeSpinnerShoe1);
+        spinner = (Spinner) findViewById(R.id.sizeSpinnerShoe1);
         String[] sizes = {"XS", "S", "M", "L", "XL", "XXL"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sizes);
         spinner.setAdapter(adapter);
 
+        productName = (TextView) findViewById(R.id.product_nameShoe1);
+        productPriceWomen = (TextView) findViewById(R.id.product_priceShoe1);
+        productDesc = (TextView) findViewById(R.id.product_descShoe1);
+        imageShoe = (ImageView) findViewById(R.id.productImageShoe1);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference().child("Shopping Cart");
+        storageReference = FirebaseStorage.getInstance().getReference().child("ProductImages/shoe1_0.jpg");
+
+        try {
+            final File localFile = File.createTempFile("shoe1_0", "jpg");
+            storageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(shoe1.this, "Picture Retrieved", Toast.LENGTH_LONG).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            ((ImageView) findViewById(R.id.productImageShoe1)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(shoe1.this, "Error Occurred", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //add to cart
+        addCartBtn=(FloatingActionButton) findViewById(R.id.add_shoe1_shopping_cart);
+        addCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addingToCartList();
+                quantity += 1;
+            }
+        });
+
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    maxId = (snapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        final HashMap<String,Object> cartMap=new HashMap<>();
+        cartMap.put("Image", storageReference.toString());
+        cartMap.put("Name",productName.getText().toString());
+        cartMap.put("Price", productPriceWomen.getText().toString());
+        cartMap.put("Desc", productDesc.getText().toString());
+        cartMap.put("Size", spinner.getSelectedItem().toString());
+        cartMap.put("Date", saveCurrentDate);
+        cartMap.put("Time", saveCurrentTime);
+        cartMap.put("quantity", String.valueOf(quantity+1));
+
+        mReference.child(mAuth.getCurrentUser().getUid()).child("Shoe1").setValue(cartMap);
+
+        Toast.makeText(shoe1.this, "Product Already Added Into Shopping Cart", Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/example/madassignment/shoe8.java
+++ app/src/main/java/com/example/madassignment/shoe8.java
@@ -1,30 +1,141 @@
 package com.example.madassignment;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 
 public class shoe8 extends AppCompatActivity {
 
-
+    private FloatingActionButton addCartBtn;
+    private TextView productPriceWomen, productName, productDesc;
+    private ImageView imageShoe;
     private Spinner spinner;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference mReference;
+    private StorageReference storageReference;
+    public long maxId = 0;
+    public long quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoe8);
 
-        Spinner spinner = (Spinner) findViewById(R.id.sizeSpinnerShoe8);
+        spinner = (Spinner) findViewById(R.id.sizeSpinnerShoe8);
         String[] sizes = {"XS", "S", "M", "L", "XL", "XXL"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sizes);
         spinner.setAdapter(adapter);
 
+        productName = (TextView) findViewById(R.id.product_nameShoe8);
+        productPriceWomen = (TextView) findViewById(R.id.product_priceShoe8);
+        productDesc = (TextView) findViewById(R.id.product_descShoe8);
+        imageShoe = (ImageView) findViewById(R.id.productImageShoe8);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference().child("Shopping Cart");
+        storageReference = FirebaseStorage.getInstance().getReference().child("ProductImages/shoe8_0.jpg");
+
+        try {
+            final File localFile = File.createTempFile("shoe8_0", "jpg");
+            storageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(shoe8.this, "Picture Retrieved", Toast.LENGTH_LONG).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            ((ImageView) findViewById(R.id.productImageShoe8)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(shoe8.this, "Error Occurred", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //add to cart
+        addCartBtn=(FloatingActionButton) findViewById(R.id.add_shoe8_shopping_cart);
+        addCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addingToCartList();
+                quantity += 1;
+            }
+        });
+
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    maxId = (snapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        final HashMap<String,Object> cartMap=new HashMap<>();
+        cartMap.put("Image", storageReference.toString());
+        cartMap.put("Name",productName.getText().toString());
+        cartMap.put("Price", productPriceWomen.getText().toString());
+        cartMap.put("Desc", productDesc.getText().toString());
+        cartMap.put("Size", spinner.getSelectedItem().toString());
+        cartMap.put("Date", saveCurrentDate);
+        cartMap.put("Time", saveCurrentTime);
+        cartMap.put("quantity", String.valueOf(quantity+1));
+
+        mReference.child(mAuth.getCurrentUser().getUid()).child("Shoe8").setValue(cartMap);
+
+        Toast.makeText(shoe8.this, "Product Already Added Into Shopping Cart", Toast.LENGTH_LONG).show();
+    }
 }
