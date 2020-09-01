684d0622547024e4313513be192e30001c18d342
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2020-09-02 00:17:21 +0800

    commit by zhikai

--- app/src/main/java/com/example/madassignment/CustomNavigation.java
+++ app/src/main/java/com/example/madassignment/CustomNavigation.java
@@ -12,26 +12,21 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomNavigation extends AppCompatActivity {
 
     NavigationView nav;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item)){
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_navigation);
+
+        mAuth = FirebaseAuth.getInstance();
 
         nav = (NavigationView) findViewById(R.id.navMenu);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
@@ -40,7 +35,6 @@ public class CustomNavigation extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
 
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -52,12 +46,55 @@ public class CustomNavigation extends AppCompatActivity {
                         startActivity(new Intent(CustomNavigation.this, MainPage.class));
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
-                    case R.id.about_us:
-                        Toast.makeText(getApplicationContext(), "About Us Is Open", Toast.LENGTH_SHORT).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        break;
                     case R.id.user_profile:
                         Toast.makeText(getApplicationContext(), "Profile Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, userProfile.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.product:
+                        Toast.makeText(getApplicationContext(), "Product List Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, ProductList.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.search:
+                        Toast.makeText(getApplicationContext(), "Search Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, SearchViewFromList.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.wish_list:
+                        Toast.makeText(getApplicationContext(), "Wish List Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, wishList.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.shopping_cart:
+                        Toast.makeText(getApplicationContext(), "Shopping Cart Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, shopping_cart.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.FAQ:
+                        Toast.makeText(getApplicationContext(), "FAQ Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, FAQ.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.about_us:
+                        Toast.makeText(getApplicationContext(), "About Us Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, AboutUs.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.contactUs:
+                        Toast.makeText(getApplicationContext(), "Contact Us Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, ContactUs.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.contactUsHistory:
+                        Toast.makeText(getApplicationContext(), "Contact Us History Is Open", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, ContactUsRecords.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.signOut:
+                        mAuth.signOut();
+                        Toast.makeText(getApplicationContext(), "Signed Out Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CustomNavigation.this, Login.class));
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
                 }
@@ -66,4 +103,13 @@ public class CustomNavigation extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
